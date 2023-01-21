cf9d7ab2125c3471ed2a775ab5a6f950d0e011e9
hamza <hamzaensi@gmail.com>
2023-01-21 16:53:16 +0100

    add bootstrap

--- resources/views/tasks/tasks.blade.php
+++ resources/views/tasks/tasks.blade.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>List of tasks</title>
-</head>
-<body>
-    <h3>List of tasks</h3>
-    <ul>
-       @foreach ($tasks as $task)
-           
-       <li><a href="{{route('task',['task'=>$task->id])}}">{{$task->content}}</a> </li>
-       @endforeach
-    </ul>
-</body>
-</html>
+
+@extends('layouts.master')
+@section('title')
+List of tasks 
+@endsection
+
+@section('content')
+<h3>List of tasks</h3>
+<ul>
+   @foreach ($tasks as $task)
+       
+   <li><a href="{{route('task',['task'=>$task->id])}}">{{$task->content}}</a> </li>
+   @endforeach
+</ul>   
+@endsection
+   
